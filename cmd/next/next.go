6d3b5c52861f2cd8e9c9958cd6fb0a458520c784
Mirmukhammad Mirsodikov <mirsodikov.mir@gmail.com>
2023-06-27 12:03:42 -0500

    Added path flag to the page command to override the default path.

--- cmd/next/next.go
+++ cmd/next/next.go
@@ -14,18 +14,16 @@ var rootCmd = &cobra.Command{
 	Short: "Generate code for NextJS",
 	Long:  `Generate code for NextJS`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello, World from the Next handler!")
+		fmt.Println("Unknown command. Please use `vulcan next --help` for more information.")
 	},
 }
 
 var addCmd = &cobra.Command{
 	Use:   "add [page|component|route]",
-	Short: "Add a new page, component, route, etc.",
-	Long:  `Add a new page, component, route, etc.`,
+	Short: "Add a new page, component, or route.",
+	Long:  `Add a new page, component, or route.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cwd, _ := os.Getwd()
-		fmt.Println("Hello, World from the Next add handler!")
-		fmt.Println("Current working directory:", cwd)
+		fmt.Println("Unknown command. Please use `vulcan next add --help` for more information.")
 	},
 }
 
@@ -40,10 +38,17 @@ var pageCmd = &cobra.Command{
 		}
 
 		isServerComponent, _ := cmd.Flags().GetBool("server-component")
+		desiredPath, _ := cmd.Flags().GetString("path")
 
 		pageOpts := page{
 			Name:            args[0],
 			ServerComponent: isServerComponent,
+		}
+
+		path := "app/" + pageOpts.Name + "/"
+
+		if desiredPath != "" {
+			path = "app/" + desiredPath + "/" + pageOpts.Name + "/"
 		}
 
 		fmt.Println("Creating a new page named:", pageOpts.Name, " ...")
@@ -57,14 +62,14 @@ var pageCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		err = os.MkdirAll("app/"+pageOpts.Name, 0755)
+		err = os.MkdirAll(path, 0755)
 
 		if err != nil {
 			fmt.Println("Error creating directory:", err)
 			os.Exit(1)
 		}
 
-		newFile, err := os.Create("app/" + pageOpts.Name + "/page.jsx")
+		newFile, err := os.Create(path + "/page.jsx")
 		if err != nil {
 			fmt.Println("Error creating file:", err)
 			os.Exit(1)
@@ -89,7 +94,9 @@ type page struct {
 
 func init() {
 	pageVars := page{}
+	var path string
 	pageCmd.Flags().BoolVarP(&pageVars.ServerComponent, "server-component", "s", false, "Create the page as a server component")
+	pageCmd.Flags().StringVarP(&path, "path", "p", "", "Specify the path for the page")
 
 	addCmd.AddCommand(pageCmd)
 	rootCmd.AddCommand(addCmd)

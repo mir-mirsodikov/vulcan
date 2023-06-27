c4973102204875798c51f01b66b28d61c1c436c2
Mirmukhammad Mirsodikov <mirsodikov.mir@gmail.com>
2023-06-27 09:42:24 -0500

    Added basic functionality to create a new file using the `vulcan next add page [name]` command

--- cmd/next/next.go
+++ cmd/next/next.go
@@ -3,6 +3,7 @@ package next
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var rootCmd = &cobra.Command{
@@ -12,6 +13,46 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Hello, World from the Next handler!")
 	},
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add [page|component|route]",
+	Short: "Add a new page, component, route, etc.",
+	Long:  `Add a new page, component, route, etc.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cwd, _ := os.Getwd()
+		fmt.Println("Hello, World from the Next add handler!")
+		fmt.Println("Current working directory:", cwd)
+	},
+}
+
+var pageCmd = &cobra.Command{
+	Use:   "page [name]",
+	Short: "Add a new page",
+	Long:  `Add a new page`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, World from the Next page handler!")
+
+		name := args[0]
+		if name == "" {
+			fmt.Println("Please provide a name for the page.")
+			os.Exit(1)
+		}
+
+		newFile, err := os.Create(name)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			os.Exit(1)
+		}
+		defer newFile.Close()
+	},
+}
+
+func init() {
+	pageCmd.Flags().StringP("name", "n", "", "Name of the page to add.")
+
+	addCmd.AddCommand(pageCmd)
+	rootCmd.AddCommand(addCmd)
 }
 
 func NextCommand() *cobra.Command {
